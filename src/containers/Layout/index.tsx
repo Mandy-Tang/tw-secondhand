d48e7ded97e1a19fe1d7d68ae09f7c3984edb124
Wang pei <519875872@qq.com>
2017-07-29 16:04:04 +0800

    adjusting the style of menu and add layout wrapper

---
+++ src/containers/Layout/index.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react'
+import {connect} from 'react-redux'
+import {MenuBar} from '../../components';
+
+const homeIcon = require('../../containers/resources/home.svg');
+const userIcon = require('../../containers/resources/person.svg');
+const addIcon = require('../../containers/resources/Very-Basic-Plus-icon.svg');
+
+const Layout = ({children}) => {
+  const menus = [
+    {
+      id: 'home',
+      icon: homeIcon,
+      path: ""
+    },
+    {
+      id: 'add',
+      icon: addIcon
+    },
+    {
+      id: 'user',
+      icon: userIcon,
+      path: 'about-us'
+    }
+  ];
+  return (
+    <div className="layoutWrapper">
+      {children}
+      <MenuBar items={menus}/>
+    </div>
+  )
+}
+
+const ConnectedLayout = connect()(Layout)
+
+const layoutWrapper = InnerContent => (
+  function MenuLayout(props) {
+    return (
+      <ConnectedLayout {...props}>
+        <InnerContent {...props}/>
+      </ConnectedLayout>
+    )
+  }
+)
+
+export {layoutWrapper};
+
+export default ConnectedLayout;
--- src/containers/pages/Home/HomePage.tsx
+++ src/containers/pages/Home/HomePage.tsx
@@ -3,37 +3,16 @@ import { connect, DispatchProp } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
 
 import * as D from '../../../definitions';
-import { userLogin } from '../../../modules/user/actions';
-import { Logo, ProductItem, Button, MenuBar }from '../../../components';
+import { Logo, ProductItem, Button }from '../../../components';
 
 import './HomePage.css';
-
-const homeIcon = require('./../../resources/home.svg');
-const userIcon = require('./../../resources/person.svg');
-const addIcon = require('./../../resources/Very-Basic-Plus-icon.svg');
+import {layoutWrapper} from "../../Layout/index";
 
 type HomePageProps<S> = DispatchProp<S> & RouteComponentProps<S> & {
     user: D.UserState
 };
 
 const HomePage = (props: HomePageProps<object>) => {
-  const menus =[
-    {
-      id: 'home',
-      icon: homeIcon,
-      path: ""
-    },
-    {
-      id: 'add',
-      icon: addIcon
-    },
-    {
-      id: 'user',
-      icon: userIcon,
-      path: 'about-us'
-    }
-    ];
-    const { dispatch, user } = props;
     return (
         <div className="App">
             <div className="App-header">
@@ -43,11 +22,6 @@ const HomePage = (props: HomePageProps<object>) => {
             <p className="App-intro">
                 To get started, edit <code>src/App.tsx</code> and save to reload.
             </p>
-            <p>{user.name ? `User Name: ${user.name}` : 'No User Name.'}</p>
-
-            <button onClick={() => dispatch(userLogin({username: 'admin', password: 'admin'}))}>
-                Login and get User Name
-            </button>
             <p>
                 <Button destination='about-us' text='Go to About Us' />
             </p>
@@ -62,11 +36,15 @@ const HomePage = (props: HomePageProps<object>) => {
             price={3000}
             img="http://cdn2.gsmarena.com/vv/pics/apple/apple-iphone-7-1.jpg"
             status={0}/>
-          <MenuBar items={menus}/>
+          <ProductItem
+            name="iphone 6s"
+            price={3000}
+            img="http://cdn2.gsmarena.com/vv/pics/apple/apple-iphone-7-1.jpg"
+            status={0}/>
         </div>
     );
 };
 
-export default connect(
+export default layoutWrapper(connect(
     (state: D.RootState<object>) => ({user: state.user})
-)(HomePage);
+)(HomePage));
