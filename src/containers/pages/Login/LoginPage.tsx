2278123eca570e955ef1d7802a8e856fd68f2c97
mandy <hitangman@qq.com>
2017-07-31 08:38:15 +0800

    [#1][Mandy] add login page

---
+++ src/containers/pages/Login/LoginPage.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+
+const LoginPage = () => (
+  <div>
+    <h2>Login</h2>
+  </div>
+);
+
+export default connect()(LoginPage);
--- src/store/routers.tsx
+++ src/store/routers.tsx
@@ -3,12 +3,14 @@ import { Route } from 'react-router';
 
 import HomePage from '../containers/pages/Home/HomePage';
 import AboutUsPage from '../containers/pages/AboutUs/AboutUsPage';
+import LoginPage from '../containers/pages/Login/LoginPage';
 
 const router = (
-    <div>
-        <Route exact={true} path="/" component={HomePage}/>
-        <Route path="/about-us" component={AboutUsPage}/>
-    </div>
+  <div>
+    <Route exact={true} path="/" component={HomePage}/>
+    <Route path="/about-us" component={AboutUsPage}/>
+    <Route path="/login" component={LoginPage}/>
+  </div>
 );
 
 export default router;
