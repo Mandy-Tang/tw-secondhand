29434db7caac52b640fe9db2ee510d68099413e5
mandy <hitangman@qq.com>
2017-08-03 13:22:03 +0800

    [#1][Mandy] user fetch

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -2,4 +2,10 @@ import * as D from '../definitions';
 import { fetchJson } from './utils';
 import { USER_LOGIN_API } from './urls';
 
-export const login = (user: D.UserForLogin): Promise<D.User> => fetchJson(USER_LOGIN_API, user);
+export const login = (user: D.UserForLogin): Promise<D.User> => fetchJson(USER_LOGIN_API, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(user)
+});
