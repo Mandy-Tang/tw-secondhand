a04d5c4be16edff8d7500cc4d2115df017b3a4e9
Wang pei <519875872@qq.com>
2017-08-05 13:44:20 +0800

    adjust release style

--- src/containers/pages/Release/index.tsx
+++ src/containers/pages/Release/index.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react'
 import FileUpload from "../../../components/FileUpload/index";
+import './index.css'
+import { connect } from "react-redux";
+import { PopUpWrapper } from "../../Layout/Popup/PopUp";
+import Header from "../../../components/Header/Header";
+import Button from "../../../components/Button/Button";
 
 class ReleasePage extends React.Component {
   constructor(props) {
@@ -7,10 +12,24 @@ class ReleasePage extends React.Component {
   }
 
   render() {
-    return <div>
-      <FileUpload/>
-    </div>
+    return (
+      <div>
+        <Header title="发布宝贝"/>
+        <FileUpload/>
+        <form action="" method="post">
+          <div className="inputGroup">
+            <input name="name" type="text" placeholder="商品名称"/>
+            <input name="price" type="text" placeholder="售价￥"/>
+            <textarea
+              name="description"
+              placeholder="添加描述..."
+              rows={10} cols={30}
+            />
+          </div>
+          <Button destination="" text="开始出售"/>
+        </form>
+      </div>)
   }
 }
 
-export default ReleasePage;
+export default PopUpWrapper(connect()(ReleasePage));
