415efcbd69d957fbea4e816ecad3c0807666b85e
mandy <hitangman@qq.com>
2017-08-08 11:43:23 +0800

    [#1][#11][#19][Mandy] complete route feature in login,logout,signup

--- src/modules/user/actions.ts
+++ src/modules/user/actions.ts
@@ -1,5 +1,6 @@
 import { fromPromise } from 'most';
 import { select, Epic } from 'redux-most';
+import { goBack, push } from 'react-router-redux';
 
 import * as D from '../../definitions';
 import { login, signUp, logout } from '../../apis/user';
@@ -17,30 +18,35 @@ export const userLogin = (user: D.UserForLogin): D.UserAction => ({ type: USER_L
 export const userSignUp = (user: D.UserForLogin): D.UserAction => ({ type: USER_SIGN_UP, payload: user});
 export const userLogout = (): D.UserAction => ({ type: USER_LOGOUT});
 
-const loginEpic: Epic<D.GeneralAction> = (action$) => action$.thru(select(USER_LOGIN))
+const loginEpic: Epic<D.GeneralAction> = (action$, store) => action$.thru(select(USER_LOGIN))
     .chain((action: D.UserAction) => fromPromise(login(action.payload)))
     .map((loginResponse: null | D.UserForLoginResponse) => {
       window.localStorage.setItem('username', loginResponse.username);
       window.localStorage.setItem('sessionToken', loginResponse.sessionToken);
-      return (
-        loginResponse
-          ? {type: USER_LOGIN_SUC, payload: loginResponse}
-          : {type: USER_LOGIN_FAIL}
-      );
+      if (loginResponse) {
+        store.dispatch(goBack());
+        return {type: USER_LOGIN_SUC, payload: loginResponse};
+      } else {
+        return {type: USER_LOGIN_FAIL};
+      }
     });
-const signUpEpic: Epic<D.GeneralAction> = (action$) => action$.thru(select(USER_SIGN_UP))
+const signUpEpic: Epic<D.GeneralAction> = (action$, store) => action$.thru(select(USER_SIGN_UP))
   .chain((action: D.UserAction) => fromPromise(signUp(action.payload)))
-  .map((signUpResponse: null | D.User) => (
-    signUpResponse
-      ? {type: USER_SIGN_UP_SUC, payload: signUpResponse}
-      : {type: USER_SIGN_UP_FAIL}
-  ));
+  .map((signUpResponse: null | D.User) => {
+    if (signUpResponse) {
+      store.dispatch(push('/profile'));
+      return {type: USER_SIGN_UP_SUC, payload: signUpResponse};
+    } else {
+      return {type: USER_SIGN_UP_FAIL};
+    }
+  });
 
-const logoutEpic: Epic<D.GeneralAction> = (action$) => action$.thru(select(USER_LOGOUT))
+const logoutEpic: Epic<D.GeneralAction> = (action$, store) => action$.thru(select(USER_LOGOUT))
   .chain((action: D.UserAction) => fromPromise(logout()))
   .map(() => {
     window.localStorage.setItem('username', '');
     window.localStorage.setItem('sessionToken', '');
+    store.dispatch(push('/'));
     return {type: USER_LOGOUT_SUC};
   });
 
