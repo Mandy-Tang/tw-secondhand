8e6eaa63c02556e15e43bf099a10aff0c1b503b5
Wang pei <519875872@qq.com>
2017-08-09 11:43:36 +0800

    apply verify

--- src/containers/pages/Release/index.tsx
+++ src/containers/pages/Release/index.tsx
@@ -3,15 +3,16 @@ import FileUpload from '../../../components/FileUpload/index';
 import './index.css';
 import { connect, DispatchProp } from 'react-redux';
 import { PopUpWrapper } from '../../Layout/Popup/PopUp';
-import { Button, Header, CheckLogin } from '../../../components';
+import { Header, CheckLogin } from '../../../components';
 import { RouteComponentProps } from 'react-router';
 import { uploadImageActionCreator, createProductActionCreator } from '../../../modules/product/actions';
+import { withRouter } from "react-router-dom";
 
 export type PageProps<S> = DispatchProp<S> & RouteComponentProps<S> & {
   uploadedImg?: string
 };
 
-class ReleasePage extends React.Component<PageProps<Object>> {
+class ReleasePage extends React.Component<PageProps<Object>, { warningMsg?: boolean }> {
   constructor(props) {
     super(props);
     this.state = {};
@@ -28,7 +29,17 @@ class ReleasePage extends React.Component<PageProps<Object>> {
       ...this.state,
       "img": this.props.uploadedImg,
     };
-    this.props.dispatch(createProductActionCreator(postData));
+    debugger;
+    if (this.checkField(postData)) {
+      this.props.dispatch(createProductActionCreator(postData));
+      this.props.history.push('/');
+    } else {
+      this.setState({warningMsg: true})
+    }
+  }
+
+  private checkField(postData) {
+    return postData.name && postData.price && postData.description && postData.img ? true : false;
   }
 
   updateValueToState = (key) => (e) => {
@@ -39,10 +50,10 @@ class ReleasePage extends React.Component<PageProps<Object>> {
 
   render() {
     return (
-      <div>
+      <div className="releasePage">
         <Header title="发布宝贝"/>
         <FileUpload onImageUpload={this.handleImageUpload} uploadedImage={this.props.uploadedImg}/>
-        <form action="" method="post">
+        <div>
           <div className="inputGroup">
             <input name="name" type="text" placeholder="商品名称" onChange={this.updateValueToState('name')}/>
             <input name="price" type="text" placeholder="售价￥" onChange={this.updateValueToState('price')}/>
@@ -52,9 +63,12 @@ class ReleasePage extends React.Component<PageProps<Object>> {
               rows={10} cols={30}
               onChange={this.updateValueToState('description')}
             />
+            {this.state.warningMsg ? <span>全部选项必填</span> : ''}
           </div>
-          <Button destination="" text="开始出售" handleClick={this.handlePostProduct}/>
-        </form>
+          <button className="button" onClick={this.handlePostProduct}>
+            开始出售
+          </button>
+        </div>
       </div>)
   }
 }
@@ -63,5 +77,6 @@ const mapStateToProps = (state) => {
   return {
     uploadedImg: state.uploadImg,
   };
-}
-export default CheckLogin(PopUpWrapper(connect(mapStateToProps)(ReleasePage)));
+};
+export default CheckLogin(PopUpWrapper(connect(mapStateToProps)(withRouter(ReleasePage))));
+
